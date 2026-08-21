3e0c521a19153cb71b53322c8158e17939434668
shividixit <shivani.midis@gmail.com>
2026-08-21 14:48:34 +0530

    navbar

--- components/views/VenueView.tsx
+++ components/views/VenueView.tsx
@@ -15,6 +15,7 @@ export default function VenueView({ onNavigate }: VenueViewProps) {
       image: "/assets/images (3).jpeg",
       link: "https://www.hilton.com/en/hotels/bctbuqq-waterstone-resort-and-marina-boca-raton/?WT.mc_id=zLADA0AA1QQ2PSH3GGL4AMPHTBRD_Oct5GNPLTPMWBRNLAB6BCTBUQQ7EN8i142435__z22150162559z__&gclsrc=aw.ds&&utm_campaign=%7Bcampaign%7D&utm_adgroup=%7Badgroup%7D&utm_keyword=waterstone%20resort%20%26%20marina%20boca%20raton%20curio%20collection%20by%20hilton&gad_source=1&gad_campaignid=22150162559&gbraid=0AAAAA-Q0XEbzEk5RoeOwx95koEJq-JLh_&gclid=EAIaIQobChMIqrPw7f-nlgMVuIDCCB10DwkMEAAYASAAEgJBevD_BwE",
     },
+
     {
       name: "The Embassy Suites",
       dist: "4.6 km",
